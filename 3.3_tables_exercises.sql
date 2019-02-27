35ea262497c340cbc1b6b73a659792eac578926c
Eric Escalante <eric.m.escalante@gmail.com>
2019-02-27 14:21:20 -0600

    SQL - Tables

---
+++ 3.3_tables_exercises.sql
@@ -0,0 +1,39 @@
+
+-- Use the employees database
+USE employees;
+-- List all the tables in the database
+SHOW TABLES;
+-- Explore the employees table
+DESCRIBE employees;
+/* 5. What different data types are present on this table?
+ * int, date, varchar, enum
+ *
+ * 6. Which table(s) do you think contain a numeric type column?
+ * emp_no
+ *
+ * 7. Which table(s) do you think contain a string type column?
+ * first_name, last_name
+ *
+ * 8. Which table(s) do you think contain a date type column?
+ * birth_date, hire_date
+ *
+ * 9. What is the relationship between the employees and the departments tables?
+ *
+ * employee number & department number, along with the from and to date in that specific department
+ *
+ * 10. Show the SQL that created the dept_manager table.
+ */
+ SHOW CREATE TABLE dept_manager;
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
